8945d1d6c6e8a63756fb05ea0a4da522af9c52cb
Tracy Reuther <tracyreuther@gmail.com>
2019-08-25 18:04:53 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,21 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+
+    resp.write "#{num_1}\n"   # Write replaces puts
+    resp.write "#{num_2}\n"   # Write replaces puts
+    resp.write "#{num_3}\n"   # Write replaces puts
+
+    if num_1 == num_2 && num_2 == num_3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+
+    resp.finish             #Response does not run until code hits "finish"
   end
 
 end
