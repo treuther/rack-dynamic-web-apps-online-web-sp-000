ac7d4c1fcf598cd5e101e9116cf808f63054229e
Tracy Reuther <tracyreuther@gmail.com>
2019-08-25 17:54:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,13 @@ class Application
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
+
+    if num ++ num && num_2 == num_3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+
     resp.finish
   end
 
